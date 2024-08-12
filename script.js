e28887283ac05ee7de3cb46f9af1bef3d0427bf6
Rafa195 <rafa.ro195@gmail.com>
2024-08-11 22:51:03 -0300

    att domingo
    Signed-off-by: Rafa195 <rafa.ro195@gmail.com>

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ window.addEventListener('scroll', function() {
         footer.style.bottom = '-100px'; // Esconde o footer
     }
 });
+.description {
+    font-size: clamp(1rem, 2.5vw, 1.5rem); /* Ajusta o tamanho da fonte de acordo com a largura da tela */
+    margin: clamp(2rem, 4vw, 4rem) 0; /* Ajusta a margem superior e inferior */
+    line-height: 1.5;
+  }
+  
